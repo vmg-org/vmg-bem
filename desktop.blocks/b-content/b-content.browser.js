42b12d7864e6fed5dc3924d3d0e721be8bb48a16
IvanRave <reyvart@hotmail.com>
2014-09-27 13:54:22 +0400

    chore(release): v0.1.0
    clone from BEM stub; add settings
    
    time: 0h:25

---
+++ desktop.blocks/b-content/b-content.browser.js
@@ -0,0 +1,8 @@
+/* global modules:false */
+
+modules.define('b-content', function(provide) {
+
+provide();
+
+});
+
---
+++ desktop.bundles/index/index.bemjson.js
@@ -0,0 +1,13 @@
+({
+    block: 'page',
+    title: 'index',
+    head: [
+        { elem: 'css', url: '_index.css' }
+    ],
+    scripts: [{ elem: 'js', url: '_index.js' }],
+    content: [
+       {
+           block: 'b-content'
+       }
+    ]
+})
